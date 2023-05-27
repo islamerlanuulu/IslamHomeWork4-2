355d6131b091347842da74032398c21bfb4f7d70
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-05-27 13:59:14 +0600

    Initial Commit

---
+++ IslamHomeWork4-2/IslamHomeWork4_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IslamHomeWork4_2App.swift
+//  IslamHomeWork4-2
+//
+//  Created by MacBook Pro on 27/5/23.
+//
+
+import SwiftUI
+
+@main
+struct IslamHomeWork4_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
